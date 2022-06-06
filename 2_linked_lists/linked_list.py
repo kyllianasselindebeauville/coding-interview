8b73a9f8fd78f83fac41ef9b3b9f440e4b1a9218
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-06-06 20:10:29 +0200

    Add LinkedListNode

---
+++ 2_linked_lists/linked_list.py
@@ -0,0 +1,8 @@
+class LinkedListNode:
+    def __init__(self, data, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.data)
