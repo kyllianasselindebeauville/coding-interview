0987e66233ee4f4d7c38b15f63514c42fc78c6b7
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-06-18 20:19:47 +0200

    Add 2.2 Return Kth to Last

---
+++ 2_linked_lists/2_return_kth_to_last.py
@@ -0,0 +1,73 @@
+# 2.2 Return Kth to Last
+
+from linked_list import LinkedList
+
+
+def kth_to_last(ll, k):
+    """Finds the kth to last element of a linked list.
+
+    Iterates through the linked list using two iterators separated by a
+    distance k to find the kth to last element.
+
+    Args:
+        ll: A linked list.
+        k: An integer representing the K in "Kth to last element".
+
+    Returns:
+        The Kth to last element if it exists, None otherwise.
+    """
+    current = runner = ll.head
+
+    for _ in range(k):
+        if runner:
+            runner = runner.next
+        else:
+            return None
+
+    while runner:
+        current, runner = current.next, runner.next
+
+    return current
+
+
+def kth_to_last_with_len(ll, k):
+    """Finds the kth to last element of a linked list.
+
+    Iterates through the linked list to find the element at the searched index.
+
+    Args:
+        ll: A linked list.
+        k: An integer representing the K in "Kth to last element".
+
+    Returns:
+        The Kth to last element if it exists, None otherwise.
+    """
+    length = len(ll)
+
+    if k <= 0 or k > length:
+        return None
+
+    for index, current in enumerate(ll):
+        if index == length - k:
+            return current
+
+
+def main():
+    print('Enter the elements of the linked list, then press Enter: ')
+    ll = LinkedList()
+
+    while True:
+        input_ = input()
+
+        if input_:
+            ll.append(input_)
+        else:
+            break
+
+    print(ll)
+    k = int(input('Enter the value of K: '))
+    print(f'The {k}th to last element is {kth_to_last(ll, k)}')
+
+
+if __name__ == '__main__':
+    main()
