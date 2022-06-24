de3645a25c89272efd9ebd89665c2047a3c64091
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-06-24 14:52:42 +0200

    Add 2.4 Partition

---
+++ 2_linked_lists/4_partition.py
@@ -0,0 +1,33 @@
+# 2.4 Partition
+
+from linked_list import LinkedList
+
+
+def partition(ll, x):
+    current = ll.tail = ll.head
+
+    while current:
+        next_node = current.next
+
+        if current.data < x:
+            current.next, ll.head = ll.head, current
+        else:
+            ll.tail.next, ll.tail = current, current
+
+        current = next_node
+
+    ll.tail.next = None
+
+
+def main():
+    ll = LinkedList()
+    ll.input(int)
+    print(ll)
+
+    x = int(input('Enter the value of X: '))
+    partition(ll, x)
+    print(ll)
+
+
+if __name__ == '__main__':
+    main()
