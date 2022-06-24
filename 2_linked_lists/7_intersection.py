9ddd81c58fd48dde5999b8fc9541a118ae864b83
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-06-24 20:49:13 +0200

    Add 2.7 Intersection

---
+++ 2_linked_lists/7_intersection.py
@@ -0,0 +1,39 @@
+# 2.7 Intersection
+
+from linked_list import LinkedList
+
+
+def intersection(ll1, ll2):
+    if ll1.tail is not ll2.tail:
+        return None
+
+    current1, current2 = ll1.head, ll2.head
+
+    for _ in range(abs(len(ll1) - len(ll2))):
+        if len(ll1) > len(ll2):
+            current1 = current1.next
+        else:
+            current2 = current2.next
+
+    while current1 is not current2:
+        current1, current2 = current1.next, current2.next
+
+    return current1
+
+
+def main():
+    ll1, ll2, ll3 = LinkedList(), LinkedList(), LinkedList()
+    ll1.input()
+    ll2.input()
+    ll3.input()
+
+    ll1.tail.next, ll1.tail = ll3.head, ll3.head
+    ll2.tail.next, ll2.tail = ll3.head, ll3.head
+
+    print(ll1)
+    print(ll2)
+    print(f'The intersecting node is: {intersection(ll1, ll2)}')
+
+
+if __name__ == '__main__':
+    main()
