1bbb0a4b9dcc0cde2fb715969263aa1caa6261d1
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-07-19 22:31:57 +0200

    Add BinaryTree

--- 04_trees_and_graphs/tree.py
+++ 04_trees_and_graphs/tree.py
@@ -8,3 +8,20 @@ class BinaryTreeNode:
 
     def __str__(self):
         return str(self.data)
+
+class BinaryTree:
+    def __init__(self, root=None):
+        self.root = root
+
+    def __str__(self):
+        return self.str_representation(self.root)
+
+    def str_representation(self, node, indent=0):
+        if node is None:
+            return ''
+
+        return str(
+            self.str_representation(node.right, indent + 1) +
+            indent * '\t' + str(node) + '\n' +
+            self.str_representation(node.left, indent + 1)
+        )
