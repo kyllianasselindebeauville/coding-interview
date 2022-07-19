325d2fefe7f9266598b3f7350b67c8bf93def33f
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-07-19 20:16:12 +0200

    Add BinaryTreeNode

---
+++ 04_trees_and_graphs/tree.py
@@ -0,0 +1,10 @@
+# Tree
+
+class BinaryTreeNode:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.data)
